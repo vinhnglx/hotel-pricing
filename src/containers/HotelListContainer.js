d59f890bef327c2c2a6fd2e9469be8ccad58459c
Vincent <vinh.nglx@gmail.com>
2019-07-07 23:28:50 +0800

    Pricing list - Saving information

--- src/containers/HotelListContainer.js
+++ src/containers/HotelListContainer.js
@@ -1,86 +1,98 @@
 import React from 'react';
-import { List, Card, Row, Col, Rate, Badge } from 'antd';
+import { List, Card, Row, Col, Rate, Badge, Tooltip, Icon } from 'antd';
+import take from 'lodash/take';
 import withDataFetching from '../hoc/withDataFetching';
-// import DataFetching from '../gateways/DataFetching';
-
-const listData = [];
-for (let i = 0; i < 10; i++) {
-  listData.push({
-    href: `/hotels/${i}`,
-    title: `Shinagawa Prince Hotel ${i}`,
-    thumbnail: 'https://d2ey9sqrvkqdfs.cloudfront.net/ZqSQ/i1_t.jpg',
-    stars: 4,
-    rating: 9.1,
-    address:
-      '107-6245 Tokyo Prefecture, Minato-ku, Akasaka 9-7-1 Tokyo Midtown, Japan'
-  });
-}
+import {
+  sortPricing,
+  taxInclusive,
+  MAX_PRICING_ITEM
+} from '../utilities/helper';
 
 const HotelListContainer = props => {
   const {
-    store: { data }
+    store: { data },
+    currency
   } = props;
 
-  console.log('data');
+  console.log('data', data);
+
   return (
     <List
       itemLayout="vertical"
       size="large"
       dataSource={data}
-      renderItem={item => (
-        <List.Item
-          key={item.id}
-          actions={[
-            <Badge count={item.rating} />,
-            <span>Location: {item.address}</span>
-          ]}
-          extra={<img width={272} alt="logo" src={item.photo} />}
-        >
-          <List.Item.Meta
-            title={
-              <a href={`/hotels/${item.id}`}>
-                <p className="mb-0">{item.name}</p>
-                <Rate disabled defaultValue={item.stars} />
-              </a>
-            }
-          />
-          <Row gutter={16}>
-            <Col span={4}>
-              <Card size="small" title="Booking.com" bordered={false}>
-                S$ 100
-              </Card>
-            </Col>
-            <Col span={4}>
-              <Card size="small" title="Hotels.com" bordered={false}>
-                S$ 100
-              </Card>
-            </Col>
-            <Col span={4}>
-              <Card size="small" title="Expedia" bordered={false}>
-                S$ 100
-              </Card>
-            </Col>
-            <Col span={4}>
-              <Card size="small" title="Getaroom" bordered={false}>
-                S$ 100
-              </Card>
-            </Col>
-            <Col span={4}>
-              <Card size="small" title="AMOMA" bordered={false}>
-                S$ 100
-              </Card>
-            </Col>
-            <Col span={4}>
-              <Card size="small" title="Prestigia" bordered={false}>
-                S$ 100
-              </Card>
-            </Col>
-          </Row>
-        </List.Item>
-      )}
+      renderItem={item => {
+        let sortedPricing;
+
+        if (item.taxes_and_fees) {
+          sortedPricing = taxInclusive(sortPricing(item), item.taxes_and_fees);
+        } else {
+          sortedPricing = sortPricing(item);
+        }
+        return (
+          <List.Item
+            key={item.id}
+            actions={[
+              <Badge count={item.rating} />,
+              <span>Location: {item.address}</span>
+            ]}
+            extra={<img width={272} alt="logo" src={item.photo} />}
+          >
+            <List.Item.Meta
+              className="ps-r"
+              title={
+                <a href={`/hotels/${item.id}`}>
+                  <p className="mb-0">
+                    {item.name}
+                    {item.taxes_and_fees && (
+                      <span className="tooltip">
+                        <Tooltip placement="right" title="Tax-Inclusive">
+                          <Icon type="question-circle-o" />
+                        </Tooltip>
+                      </span>
+                    )}
+                  </p>
+
+                  <Rate disabled defaultValue={item.stars} />
+                </a>
+              }
+            />
+            <Row gutter={16}>
+              {take(Object.keys(sortedPricing), MAX_PRICING_ITEM).map(key => {
+                const hotelPrice = sortedPricing[key];
+                const maxPrice = item.maxCompetitor;
+
+                return (
+                  <Col span={4} key={key}>
+                    <Card
+                      size="small"
+                      title={key}
+                      bordered={false}
+                      className="ps-r"
+                    >
+                      {(hotelPrice && `${currency}${hotelPrice}`) ||
+                        'Rate Unavailable'}
+                      {maxPrice && key === 'Hotel' && hotelPrice < maxPrice && (
+                        <span className="tooltip">
+                          <Tooltip
+                            placement="right"
+                            title={`Saving ${100 -
+                              (hotelPrice / maxPrice) * 100}%`}
+                          >
+                            <Icon type="question-circle-o" />
+                          </Tooltip>
+                        </span>
+                      )}
+                    </Card>
+                  </Col>
+                );
+              })}
+            </Row>
+          </List.Item>
+        );
+      }}
     />
   );
 };
 
 export default withDataFetching(HotelListContainer);
-// export default enhance(HotelListContainer);
--- src/utilities/helper.js
+++ src/utilities/helper.js
@@ -1,8 +1,10 @@
 // eslint-disable-next-line import/prefer-default-export
 
-import values from 'lodash/values';
-import filter from 'lodash/filter';
-import orderBy from 'lodash/orderBy';
+// import values from 'lodash/values';
+// import filter from 'lodash/filter';
+// import orderBy from 'lodash/orderBy';
+
+import _ from 'lodash';
 
 export const currenciesList = {
   $: 'USD',
@@ -17,6 +19,8 @@ export const HOTELS_DATA_URL =
 export const PRICE_DATA_URL =
   'http://5c08f37bea3172001389ccbd.mockapi.io/hotels/tokyo/1';
 
+export const MAX_PRICING_ITEM = 6;
+
 // Remove prices if the hotel do not exist
 export const filterPricesWithHotelIds = (prices, hotelIds) => {
   return prices.filter(item => {
@@ -26,22 +30,45 @@ export const filterPricesWithHotelIds = (prices, hotelIds) => {
 
 // Matching hotels and prices based on ID
 export const matchingHotelsWithPrices = (hotels, prices) => {
-  return values(
-    hotels.concat(prices).reduce((acc, x) => {
-      acc[x.id] = Object.assign(acc[x.id] || {}, x);
-      return acc;
-    }, {})
-  );
+  // Combine by id
+  const comb = hotels.concat(prices).reduce((acc, x) => {
+    acc[x.id] = Object.assign(acc[x.id] || {}, x);
+    return acc;
+  }, {});
+
+  // Add maxCompetitor pricing if there is
+  // BUG: If there is tax then values must be calculated with tax before get max
+  const updatedData = _.forOwn(comb, (value, key) => {
+    let maxCompetitor;
+    // let newValue;
+    if (value.competitors) {
+      const taxesFees = value.taxes_and_fees;
+      const valuesCompetitors = taxesFees
+        ? _.map(
+            _.values(value.competitors),
+            el => el + taxesFees.tax + taxesFees.hotel_fees
+          )
+        : _.values(value.competitors);
+      maxCompetitor = _.max(valuesCompetitors);
+    }
+    // value.maxCom = maxCompetitor;
+    const newValue = value;
+    newValue.maxCompetitor = maxCompetitor;
+
+    return newValue;
+  });
+
+  return _.values(updatedData);
 };
 
 // Hotel sorting by prices
 export const sortHotels = hotels => {
-  const nonPricingHotels = filter(hotels, el => {
+  const nonPricingHotels = _.filter(hotels, el => {
     return !el.price;
   });
 
-  const pricingHotelsDesc = orderBy(
-    filter(hotels, el => {
+  const pricingHotelsDesc = _.orderBy(
+    _.filter(hotels, el => {
       return el.price;
     }),
     ['price'],
@@ -49,4 +76,28 @@ export const sortHotels = hotels => {
   );
 
   return pricingHotelsDesc.concat(nonPricingHotels);
+};
+
+// Pricing sorting in a hotel
+export const sortPricing = hotel => {
+  const { price, competitors = {} } = hotel;
+  competitors.Hotel = price;
+
+  const pairs = Object.entries(competitors);
+
+  const sortedPairs = pairs.sort((a, b) => a[1] - b[1]);
+
+  return _.fromPairs(sortedPairs);
+};
+
+// Including tax and fee if there is
+export const taxInclusive = (pricing, taxFee) => {
+  const { tax, hotel_fees } = taxFee;
+  const pricingTaxInclusive = {};
+
+  Object.keys(pricing).forEach(key => {
+    pricingTaxInclusive[key] = pricing[key] + tax + hotel_fees;
+  });
+
+  return pricingTaxInclusive;
 };
